e3793479466f2871b9bd72287e48450990374d3d
ShouryaSharma277 <shouryasharmadevelopment@gmail.com>
2022-02-12 22:59:18 +0530

    more fixes

--- src/components/todos/TodoStorage.jsx
+++ src/components/todos/TodoStorage.jsx
@@ -25,15 +25,14 @@ export const TodoStorage = ({ setTodos, todos, text, todo }) => {
 
     return (
         <div className={`dynamicCss ${todo.completed ? "dynamicCssCompleted" : ""}`}>
-            <div
-                className="flex mb-5 flex-col items-center rounded-lg dark:bg-neutral-800 dark:text-white border-none shadow-md md:flex-row md:max-w-xl" id="main">
-                <div className="flex flex-col md:flex-row justify-between p-4 leading-normal w-80 e">
-                    <li className="mb-2 mr-40 text-lg">{text}</li>
+            <div className='flex mb-5 flex-col items-center dark:bg-neutral-800 dark:text-white shadow-md md:flex-row md:max-w-xl'>
+                <div className='flex flex-col md:flex-row justify-between p-4 lg:w-96 md:w-80 '>
+                    <li className='mb-2 mr-40 text-lg'>{text}</li>
                     <div className='flex space-x-4 text-lg'>
-                        <button className="my-2" style={{ color: "#2ba13d" }} onClick={completeHandler}>
+                        <button className='my-2' style={{ color: "#2ba13d" }} onClick={completeHandler}>
                             <FontAwesomeIcon icon={faCircleCheck} />
                         </button>
-                        <button className="my-2" style={{ color: "#a32c2c" }} onClick={deleteHandler}>
+                        <button className='my-2' style={{ color: "#a32c2c" }} onClick={deleteHandler}>
                             <FontAwesomeIcon icon={faMinusCircle} />
                         </button>
                     </div>
